d859a833de346d622c970de5f26adfe3e750dad5
Selim Şahin <1231007@dogus.edu.tr>
2018-10-12 18:01:20 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sensors Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool verify = true;
+  double oldX = -1.0;
+  double oldZ = -1.0;
+  bool oc = true;
+
+  void _incrementCounter() {
+    setState(() {
+      if (oc) {
+        oc = false;
+        print('açık');
+      } else {
+        for (int i = 0; i <= list.length; i++) {
+          print('${list[i]}');
+        }
+      }
+    });
+  }
+
+  List<double> _accelerometerValues;
+  List<double> _userAccelerometerValues;
+  List<double> _gyroscopeValues;
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+  List<dynamic> list = <dynamic>[];
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> accelerometer =
+        _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
+    final List<String> gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1))?.toList();
+    final List<String> userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        ?.toList();
+
+    double oldX = _accelerometerValues[0];
+
+    if (oldZ == -1) {
+      oldZ = _accelerometerValues[2];
+    }
+
+    if (oldZ - _accelerometerValues[2] < -8 ||
+        oldZ - _accelerometerValues[2] > 8) {
+      verify = true;
+    }
+    if (verify && _accelerometerValues[2] < 10 && _accelerometerValues[2] > 8) {
+      _counter++;
+      verify = false;
+      oldZ = _accelerometerValues[2];
+    }
+
+    if (oc == false) {
+      list.add(_accelerometerValues[2]);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sensor Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Padding(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Accelerometer: $accelerometer, $_counter'),
+                ],
+              ),
+              padding: const EdgeInsets.all(16.0),
+            ),
+            Padding(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('UserAccelerometer: $userAccelerometer'),
+                ],
+              ),
+              padding: const EdgeInsets.all(16.0),
+            ),
+            Padding(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Gyroscope: $gyroscope'),
+                ],
+              ),
+              padding: const EdgeInsets.all(16.0),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions
+        .add(accelerometerEvents.listen((AccelerometerEvent event) {
+      setState(() {
+        _accelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions.add(gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions
+        .add(userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        _userAccelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+  }
+}
